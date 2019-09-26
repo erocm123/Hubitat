c0e4054b66132183bd159e747cdf886727cf6989
erocm123 <erocmail@gmail.com>
2019-09-26 16:09:57 -0600

    Convert on / off to lowercase

--- Drivers/sonoff-2ch-tasmota.src/sonoff-2ch-tasmota.groovy
+++ Drivers/sonoff-2ch-tasmota.src/sonoff-2ch-tasmota.groovy
@@ -148,40 +148,26 @@ def parse(description) {
     
     if (result.containsKey("StatusSTS")) result = result.StatusSTS
     
-    if (result.containsKey("POWER1") || result.containsKey("POWER2") || result.containsKey("POWER3") || result.containsKey("POWER4")) {
+    if (result.containsKey("POWER1") || result.containsKey("POWER2")) {
     if (result.containsKey("POWER1")) {
         log.debug "POWER1: $result.POWER1"
         def childDevice = childDevices.find{it.deviceNetworkId == "$device.deviceNetworkId-ep1"}
         if (childDevice) {         
-            childDevice.sendEvent(name: "switch", value: result.POWER1)
+            childDevice.sendEvent(name: "switch", value: result.POWER1.toLowerCase())
         }
     }
     if (result.containsKey("POWER2")) {
         log.debug "POWER2: $result.POWER2"
         def childDevice = childDevices.find{it.deviceNetworkId == "$device.deviceNetworkId-ep2"}
         if (childDevice) {         
-            childDevice.sendEvent(name: "switch", value: result.POWER2)
-        }
-    }
-    if (result.containsKey("POWER3")) {
-        log.debug "POWER3: $result.POWER3"
-        def childDevice = childDevices.find{it.deviceNetworkId == "$device.deviceNetworkId-ep3"}
-        if (childDevice) {         
-            childDevice.sendEvent(name: "switch", value: result.POWER3)
-        }
-    }
-    if (result.containsKey("POWER4")) {
-        log.debug "POWER4: $result.POWER4"
-        def childDevice = childDevices.find{it.deviceNetworkId == "$device.deviceNetworkId-ep4"}
-        if (childDevice) {         
-            childDevice.sendEvent(name: "switch", value: result.POWER4)
+            childDevice.sendEvent(name: "switch", value: result.POWER2.toLowerCase())
         }
     }
 
     def allOff = true
     childDevices.each {
         childDevice ->
-        if (childDevice.currentState("switch").value != "OFF") allOff = false
+        if (childDevice.currentState("switch").value != "off") allOff = false
     }
 
     if (allOff) {
